1f716ee2830def3b796d957c51c55e6b0a025545
Bin-Guan <binguan2@gmail.com>
2019-07-10 23:59:11 -0400

    Initial commit

---
+++ mutect.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+module load GATK/3.8-0
+
+fam=$1
+#sbatch --cpus-per-task=8 --mem=24g --time=24:00:00 mutect.sh 2053
+#https://software.broadinstitute.org/gatk/documentation/article?id=9183
+#July 2019: 2 exome samples finished in < 14 hours and the 3rd finished <18 hours.
+
+GATK -m 24g MuTect2 \
+     -threads 8 \
+     -I:tumor ../sample_bam/$1aff.recalibrated.bam \
+     -I:normal ../sample_bam/$1unaff.recalibrated.bam \
+     --output_mode EMIT_VARIANTS_ONLY \
+     -o $1_mutect2.vcf.gz \
+     -L /data/OGVFB/OGL_NGS/bed/xgen-exome-research-panel-targets-nochr-sorted.bed \
+     -R /data/guanb/resource/GATK_resource_bundle/b37-2.8/human_g1k_v37_decoy.fasta  
---
+++ mutect_annovar_intervar.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Continued from mutect vcf.gz
+#sbatch --mem=16g --cpus-per-task=1 /data/OGVFB/OGL_NGS/variant_prioritization/src/manual_vcf_vt_intervar_VEP_vcfanno.v1.sh VCF
+set -e
+
+VCF=$1 #vcf.gz
+
+#vt
+module load vt/0.577
+
+zcat $VCF \
+| sed 's/ID=AD,Number=./ID=AD,Number=R/' \
+| vt decompose -s - \
+| vt normalize -r /data/OGVFB/resources/1000G_phase2_GRCh37/human_g1k_v37_decoy.fasta - \
+> ${VCF%.vcf.gz}.vt.vcf
+	
+module load intervar/2.1.2
+
+convert2annovar.pl -format vcf4old ${VCF%.vcf.gz}.vt.vcf -includeinfo --outfile ${VCF%.vcf.gz}.avinput
+
+table_annovar.pl ${VCF%.vcf.gz}.avinput $ANNOVAR_DATA/hg19 -buildver hg19 -remove \
+	-out ${VCF%.vcf.gz} \
+	--protocol refGene,esp6500siv2_all,1000g2015aug_all,avsnp147,dbnsfp33a,clinvar_20190305,gnomad_genome,dbscsnv11,dbnsfp31a_interpro,rmsk,ensGene,knownGene,refGeneWithVer,popfreq_max_20150413,gnomad_exome,spidex,avsnp150,spliceai_filtered \
+	-operation g,f,f,f,f,f,f,f,f,r,g,g,g,f,f,f,f,f \
+	--argument '-hgvs',,,,,,,,,,,,'-splicing 50 -hgvs',,,,, --polish -nastring . --thread $SLURM_CPUS_PER_TASK --otherinfo
+
+InterVar -i ${VCF%.vcf.gz}.avinput \
+	--input_type=AVinput -d $ANNOVAR_DATA/hg19 \
+	-o ${VCF%.vcf.gz} --skip_annovar --evidence_file=/data/OGVFB/OGL_NGS/OGL.variants.evidence.txt
+	
+
+sed "1 s/"Otherinfo"/"VCF_CHROM\\tVCF_POS\\tVCF_ID\\tVCF_REF\\tVCF_ALT\\tVCF_QUAL\\tVCF_FILTER\\tVCF_INFO\\tVCF_FORMAT\\tVCF_TUMOR\\tVCF_NORMAL"/" ${VCF%.vcf.gz}.hg19_multianno.txt > ${VCF%.vcf.gz}.annovar.txt
+
+module load R/3.5
+# #$1 = intervar ouput, $2 = annovar output, $3 = intervar_annovar_output for vcfanno (must be txt file)
+
+Rscript ~/git/pooled_family/annovar_intervar_mutect_v1.6.R ${VCF%.vcf.gz}.hg19_multianno.txt.intervar ${VCF%.vcf.gz}.annovar.txt ${VCF%.vcf.gz}.annovar.intervar.filtered.txt
+
+
+
